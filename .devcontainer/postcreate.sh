19edd07bb3fe9a21cb23267556bc7480ec3a2fa3
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2023-10-13 19:01:39 +0000

    initial renv commit

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p .renv-cache
+R -e 'install.packages("renv", repos="http://cran.us.r-project.org")'
 
 # rsync -rlFptgoa --stats .depot-cache/* /opt/julia --modify-window=1
 # julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile()'
