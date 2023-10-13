6b7992cd0a514a77430b1db2eeb46fc19c4cd325
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2023-10-13 18:28:28 +0000

    initaial commit for R container

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-mkdir -p .depot-cache
+mkdir -p .renv-cache
 
-rsync -rlFptgoa --stats .depot-cache/* /opt/julia --modify-window=1
-
-julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile()'
-
-rsync -rlFptgoa --stats /opt/julia/* .depot-cache --modify-window=1 --delete
+# rsync -rlFptgoa --stats .depot-cache/* /opt/julia --modify-window=1
+# julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile()'
+# rsync -rlFptgoa --stats /opt/julia/* .depot-cache --modify-window=1 --delete
