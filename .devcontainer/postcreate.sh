e0bc1dec6bf4bdb146d726a0ec3bbdfedb0b3093
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2023-10-13 19:10:45 +0000

    added renv restor command to conainter

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,6 +3,9 @@
 mkdir -p .renv-cache
 R -e 'install.packages("renv", repos="http://cran.us.r-project.org")'
 
+R -e 'renv:::restore()'
+
+
 # rsync -rlFptgoa --stats .depot-cache/* /opt/julia --modify-window=1
 # julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile()'
 # rsync -rlFptgoa --stats /opt/julia/* .depot-cache --modify-window=1 --delete
